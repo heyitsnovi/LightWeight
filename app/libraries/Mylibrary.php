5a424c41014ad7eaae47274d846167426b51702a
unknown <novhex94@gmail.com>
2023-08-22 13:05:15 +0800

    Add New Changes

---
+++ app/libraries/Mylibrary.php
@@ -0,0 +1,18 @@
+<?php
+/*
+* This is an example of how to make your own libraries in Lightweigh MVC Framework. 
+* You can safely delete this file but make that this file is never used in the application.
+*/
+namespace Libraries;
+
+class Mylibrary{
+
+    public function __construct(){
+        
+    }
+
+    public function output_message(){
+
+        return 'Hello World';
+    }
+}
