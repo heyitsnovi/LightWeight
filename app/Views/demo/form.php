73bf8b81ce3d56309e1eabb3729da9c2a55d0c4d
unknown <novhex94@gmail.com>
2023-08-30 14:06:03 +0800

    add new function drodpdown

--- app/Views/demo/form.php
+++ app/Views/demo/form.php
@@ -1,11 +1,17 @@
 <?php
-	$options = array(
+	$options = [
 	''				=>'',	
 	'small'         => 'Small Shirt',
 	'med'           => 'Medium Shirt',
 	'large'         => 'Large Shirt',
 	'xlarge'        => 'Extra Large Shirt',
-);
+	];
+	
+	$attributes = [
+		'id'=>'dropdown-size',
+		'class'=>'dropdown-size shirt',
+		'name'=>'shirts'
+	];
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,10 +30,14 @@
 					<?= $errors!==NULL ? $errors->first('age') : '' ?>
 				</label>
 				<br>
+				<label>
+					Size:
 				<?php
-					echo form_dropdown('shirts', $options, anti_xss($_POST['shirts']));
-					echo $errors!==NULL ? $errors->first('shirts') : '';
+					echo form_dropdown('', $options, isset($_POST['shirts']) ? anti_xss($_POST['shirts']) : '' ,$attributes );
 				?>
+				<?= $errors!==NULL ? $errors->first('shirts') : ''; ?>
+				</label>
+				<br>
 				<button type="submit"> Submit </button>
 		</form>
 	</body>
