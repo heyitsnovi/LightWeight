b0fc01edb43f2fe94144c7eae30aee8d5addc64a
unknown <novhex94@gmail.com>
2023-08-30 14:10:26 +0800

    Add Formatting

--- core/Helper.php
+++ core/Helper.php
@@ -81,165 +81,144 @@ function input_value($key) {
 }
 
 function is_ajax() {
-	
+
     return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest';
 }
 
+function base_path() {
 
-function base_path(){
-
-    $basepath =  dirname(__FILE__,2);
+    $basepath = dirname(__FILE__, 2);
 
     return anti_xss($basepath);
 }
 
-function form_dropdown($data = '', $options = array(), $selected = array(), $extra = ''){
+function form_dropdown($data = '', $options = array() , $selected = array() , $extra = '') {
 
     $defaults = array();
 
-    if (is_array($data))
-    {
-        if (isset($data['selected']))
-        {
+    if (is_array($data)) {
+        if (isset($data['selected'])) {
             $selected = $data['selected'];
-            unset($data['selected']); 
+            unset($data['selected']);
         }
 
-        if (isset($data['options']))
-        {
+        if (isset($data['options'])) {
             $options = $data['options'];
-            unset($data['options']); 
+            unset($data['options']);
         }
     }
-    else
-    {
-        $defaults = array('name' => $data);
+    else {
+        $defaults = array(
+            'name' => $data
+        );
     }
 
-    is_array($selected) OR $selected = array($selected);
-    is_array($options) OR $options = array($options);
+    is_array($selected) or $selected = array(
+        $selected
+    );
+    is_array($options) or $options = array(
+        $options
+    );
 
-   
-    if (empty($selected))
-    {
-        if (is_array($data))
-        {
-            if (isset($data['name'], $_POST[$data['name']]))
-            {
-                $selected = array($_POST[$data['name']]);
+    if (empty($selected)) {
+        if (is_array($data)) {
+            if (isset($data['name'], $_POST[$data['name']])) {
+                $selected = array(
+                    $_POST[$data['name']]
+                );
             }
         }
-        elseif (isset($_POST[$data]))
-        {
-            $selected = array($_POST[$data]);
+        elseif (isset($_POST[$data])) {
+            $selected = array(
+                $_POST[$data]
+            );
         }
     }
 
     $extra = _attributes_to_string($extra);
 
-    $multiple = (count($selected) > 1 && stripos($extra, 'multiple') === FALSE) ? ' multiple="multiple"' : '';
+    $multiple = (count($selected) > 1 && stripos($extra, 'multiple') === false) ? ' multiple="multiple"' : '';
 
-    $form = '<select '.rtrim(_parse_form_attributes($data, $defaults)).$extra.$multiple.">\n";
+    $form = '<select ' . rtrim(_parse_form_attributes($data, $defaults)) . $extra . $multiple . ">\n";
 
-    foreach ($options as $key => $val)
-    {
-        $key = (string) $key;
+    foreach ($options as $key => $val) {
+        $key = (string)$key;
 
-        if (is_array($val))
-        {
-            if (empty($val))
-            {
+        if (is_array($val)) {
+            if (empty($val)) {
                 continue;
             }
 
-            $form .= '<optgroup label="'.$key."\">\n";
+            $form .= '<optgroup label="' . $key . "\">\n";
 
-            foreach ($val as $optgroup_key => $optgroup_val)
-            {
+            foreach ($val as $optgroup_key => $optgroup_val) {
                 $sel = in_array($optgroup_key, $selected) ? ' selected="selected"' : '';
-                $form .= '<option value="'.anti_xss($optgroup_key).'"'.$sel.'>'
-                    .(string) $optgroup_val."</option>\n";
+                $form .= '<option value="' . anti_xss($optgroup_key) . '"' . $sel . '>' . (string)$optgroup_val . "</option>\n";
             }
 
             $form .= "</optgroup>\n";
         }
-        else
-        {
-            $form .= '<option value="'.anti_xss($key).'"'
-                .(in_array($key, $selected) ? ' selected="selected"' : '').'>'
-                .(string) $val."</option>\n";
+        else {
+            $form .= '<option value="' . anti_xss($key) . '"' . (in_array($key, $selected) ? ' selected="selected"' : '') . '>' . (string)$val . "</option>\n";
         }
     }
 
-    return $form."</select>\n";
+    return $form . "</select>\n";
 }
 
-	function _attributes_to_string($attributes)
-	{
-		if (empty($attributes))
-		{
-			return '';
-		}
+function _attributes_to_string($attributes) {
+    if (empty($attributes)) {
+        return '';
+    }
 
-		if (is_object($attributes))
-		{
-			$attributes = (array) $attributes;
-		}
+    if (is_object($attributes)) {
+        $attributes = (array)$attributes;
+    }
 
-		if (is_array($attributes))
-		{
-			$atts = '';
+    if (is_array($attributes)) {
+        $atts = '';
 
-			foreach ($attributes as $key => $val)
-			{
-				$atts .= ' '.$key.'="'.$val.'"';
-			}
+        foreach ($attributes as $key => $val) {
+            $atts .= ' ' . $key . '="' . $val . '"';
+        }
 
-			return $atts;
-		}
+        return $atts;
+    }
 
-		if (is_string($attributes))
-		{
-			return ' '.$attributes;
-		}
+    if (is_string($attributes)) {
+        return ' ' . $attributes;
+    }
 
-		return FALSE;
-	}
+    return false;
+}
 
-    function _parse_form_attributes($attributes, $default)
-	{
-		if (is_array($attributes))
-		{
-			foreach ($default as $key => $val)
-			{
-				if (isset($attributes[$key]))
-				{
-					$default[$key] = $attributes[$key];
-					unset($attributes[$key]);
-				}
-			}
+function _parse_form_attributes($attributes, $default) {
+    if (is_array($attributes)) {
+        foreach ($default as $key => $val) {
+            if (isset($attributes[$key])) {
+                $default[$key] = $attributes[$key];
+                unset($attributes[$key]);
+            }
+        }
 
-			if (count($attributes) > 0)
-			{
-				$default = array_merge($default, $attributes);
-			}
-		}
+        if (count($attributes) > 0) {
+            $default = array_merge($default, $attributes);
+        }
+    }
 
-		$att = '';
+    $att = '';
 
-		foreach ($default as $key => $val)
-		{
-			if ($key === 'value')
-			{
-				$val = anti_xss($val);
-			}
-			elseif ($key === 'name' && ! strlen($default['name']))
-			{
-				continue;
-			}
+    foreach ($default as $key => $val) {
+        if ($key === 'value') {
+            $val = anti_xss($val);
+        }
+        elseif ($key === 'name' && !strlen($default['name'])) {
+            continue;
+        }
 
-			$att .= $key.'="'.$val.'" ';
-		}
+        $att .= $key . '="' . $val . '" ';
+    }
 
-		return $att;
-	}
+    return $att;
+}
+
