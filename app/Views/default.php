42a778864dab9d5ce589e9665e2f086c1fbeac34
unknown <novhex94@gmail.com>
2023-09-14 14:57:02 +0800

    Add updates and fixes

--- app/Views/default.php
+++ app/Views/default.php
@@ -6,25 +6,36 @@
 	<title> Home - LightWeight</title>
 </head>
 	<body>
-		<style>
+	<style>
 			body{
-				background-color: #e9ebee;
+				background-color: #17202f;
 				font-family: 'Roboto';
 			}
 			.msg{
 				text-align: center;
 				font-size: 36px;
 				margin-top: 150px;
+				color:#05e984;
 			}
 			.text-bottom{
 				text-align: center;
+				color:white;
 			}
 			.psubtitle{
 				text-align: center;
 				font-size: 16px;
+				color:#05e984;
 			}
 			.small{
+				color:white;		
 				text-align: center;
+			}
+			.link{
+				
+				color: #62b28f;
+			}
+			.link:hover{
+				color:#05e984;
 			}
 		</style>
 	
@@ -34,7 +45,7 @@
 		<p class="psubtitle">	<?php echo get_flash('message'); ?></p>
 		<p class="small">A lightweight MVC Framework</p>
 		<p class="psubtitle">
-			<a target="_blank" href="https://github.com/heyitsnovi/LightWeight">View Project On Github</a>
+			<a target="_blank" class="link" href="https://github.com/heyitsnovi/LightWeight">View Project On Github</a>
 		</p>
  
 		<p class="text-bottom">Memory Usage : <?php  echo $data['memory_usage']; ?> @ PHP Version : <?php echo phpversion(); ?></p>
--- app/Views/errors/404.php
+++ app/Views/errors/404.php
@@ -1,29 +1,59 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">	
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto" />
-	<title>Oh No! &ndash; Error 404 </title>
+	<title>Oh No! - Error 404 </title>
 </head>
 	<body>
-		<style>
+	<style>
 			body{
-				background-color: #e9ebee;
+				background-color: #17202f;
 				font-family: 'Roboto';
 			}
 			.msg{
 				text-align: center;
 				font-size: 36px;
 				margin-top: 150px;
+				color:#05e984;
+			}
+			.text-bottom{
+				text-align: center;
+				color:white;
+			}
+			.psubtitle{
+				text-align: center;
+				font-size: 16px;
+				color:#05e984;
+			}
+			.small{
+				color:white;		
+				text-align: center;
+			}
+			.link{
+				
+				color: #62b28f;
+			}
+			.link:hover{
+				color:#05e984;
 			}
 			.credits{
-				text-align: center;
-				font-size: 10px;
+				 
+				color:white;
+				text-align:center;
+			}
+			.msg-inner{
+				color:white;
+			}
+			.img-emoji{
+				width:100px;
+				height:100px;
 			}
 		</style>
 		<div class="msg">
-			<img src="<?= base_url('assets/sad-face.svg') ?>">
-			<p>Hmmm looks like the URL you visited does not exits. Go back <a href="<?= base_url() ?>">Home</a> </p>
+			<img src="<?= base_url('assets/sad-emoji.svg') ?>" class="img-emoji">
+			<p class="msg-inner">Hmmm looks like the URL you visited does not exits. Go back <a class="link" href="<?= base_url() ?>">Home</a> </p>
 		</div>
-		<div class="credits">Sad Icon by <a href='https://iconpacks.net/?utm_source=link-attribution&utm_content=2691'>Iconpacks</a></div>
+		<div class="credits">Sad Icon by <a href='https://worldvectorlogo.com/logo/facebook-reaction-sad'>WorldVectorLogo</a></div>
 	</body>
 </html>
